60535ad411fd1577d0060e418b0b10818827d573
Paulius Mickevicius <paulius.mickevicius@twenty7tec.com>
2022-04-01 16:45:28 +0100

    removing examples

---
+++ cypress/integration/examples/one.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe("suiteOne", () => {
+  it("testOne", () => {
+    cy.visit("https://qacloud.twenty7tec.com");
+  });
+  // it("testTwo", () => {
+  //   cy.get("tr#29.link").click();
+  // });
+});
